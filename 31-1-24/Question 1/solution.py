948f32500fb9f9384b43d5345fc9ce1e42f1e79a
Anshula Killamsetty <112149471+aanshulaa@users.noreply.github.com>
2024-01-31 19:00:24 +0530

    Add files via upload

--- 31-1-24/Question 1/solution.py
+++ 31-1-24/Question 1/solution.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import re
 
 def sum_of_numbers(input_str):
-    numbers = re.findall(r'\d+', input_str)  # Find all numeric substrings
+    numbers = re.findall(r'\d+', input_str) 
     return sum(int(num) for num in numbers)
 
 def sum_of_adjacent_digits(input_str):
-    numbers = re.findall(r'\d+', input_str)  # Find all numeric substrings
+    numbers = re.findall(r'\d+', input_str) 
     return sum(int(num) for num in numbers)
 
 # Example usage:
 input_str1 = "1abd23"
 input_str2 = "Fa33ce1"
-print(sum_of_numbers(input_str1))  # Output: 24
-print(sum_of_adjacent_digits(input_str1))  # Output: 24
-print(sum_of_numbers(input_str2))  # Output: 34
-print(sum_of_adjacent_digits(input_str2))  # Output: 34
+print(sum_of_numbers(input_str1))  
+print(sum_of_adjacent_digits(input_str1)) 
+print(sum_of_numbers(input_str2)) 
+print(sum_of_adjacent_digits(input_str2)) 
 
 
 # In[ ]:
